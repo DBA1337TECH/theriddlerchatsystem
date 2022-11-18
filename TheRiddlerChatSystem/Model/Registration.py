73e6e978fe93feb57f9619811b9c46767d0d7f97
BDG_Admin <blake.degarza@gmail.com>
2022-11-18 13:10:48 -0600

    added Registration file, to the Model as a stub and place holder

---
+++ TheRiddlerChatSystem/Model/Registration.py
@@ -0,0 +1,10 @@
+"""
+DBA 1337_TECH, AUSTIN TEXAS © November 2022
+Proof of Concept code, No liabilities or warranties expressed or implied.
+"""
+
+"""
+TODO: Implement the registartion service for adding new users to store the client to the membership server.
+
+Might need to switch to a JSON implementation for ease of packet serialization.
+"""
